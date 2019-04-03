dd19a8ec662bf94b49e8909f23b2c13c73e21fb0
sarita2129 <nairsarita2129@gmail.com>
2019-04-03 14:00:29 +1100

    Readme update

--- src/component/progressbar.js
+++ src/component/progressbar.js
@@ -8,7 +8,7 @@ export default class ProgressBar extends Component{
       {this.props.percent.map( (bar) => (
                           <div key={bar} className="divprogressbarbg">
                             <div className="divprogressbar" style={{"maxWidth":"100%",width:(bar > 95) ? '95%' : bar+'%',"backgroundColor":(bar > 100) ? "red" : "rgb(43,194,83)","boxSizing":"border-box",
-                            "borderTopRightRadius": (bar > 100) ? "20px" : "8px", "borderBottomRightRadius":(bar > 100) ? "20px" : "8px"}}>
+                            "borderTopRightRadius": (bar >= 100) ? "20px" : "8px", "borderBottomRightRadius":(bar >= 100) ? "20px" : "8px"}}>
 
                             </div>
                             <span >{bar+'%'}</span>
